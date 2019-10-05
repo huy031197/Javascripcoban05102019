1e99ac13f0998822df5a87e9b4cf9ec36568cecd
Nguyễn Thanh Huy <nguyenthanhhuy@ThanhHuy.local>
2019-10-05 16:27:00 +0700

    Object

--- buoi1.js
+++ buoi1.js
@@ -4,13 +4,31 @@
 //mở môi trường thực thia control+~
 //nodemon: tự đọng thực thi lại file khi có thay đổis
 
-// 1:Khai báo biến
+
+//    1:Khai báo biến
 // var const let
-var a="Nguyen Thanh Huy"
-a = 10;
-let b = true
-b = 15;
+//var a="Nguyen Thanh Huy"
+//a = 10;
+//let b = true
+//b = 15;
 // const khong gan lai
-const c = "1000"
-c =1000
-console.log(b);
+//const c = "1000"
+//c =1000
+//console.log(b);
+//   2: Kieu du lieu
+    // primities :
+    //1-String
+    //2-Boolean
+    //3-Number
+    // special type
+    //4-null
+    //5-indefined
+    //6-Symbol
+//object
+ //1-Object
+ //2-Array
+ const thanhhuy = {
+     name : "ThanhHuy",
+     location : "Tp HCM"
+ }
+console.log(thanhhuy.name, thanhhuy.location)
