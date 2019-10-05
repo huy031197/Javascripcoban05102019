483724988622bb91175e78a93ede163cfc41d6c5
Nguyễn Thanh Huy <nguyenthanhhuy@ThanhHuy.local>
2019-10-05 16:17:48 +0700

    khai bao

---
+++ buoi1.js
@@ -0,0 +1,16 @@
+//console.log("Hello");
+//console.log("Xin chao");
+//console.log("lơp react native");
+//mở môi trường thực thia control+~
+//nodemon: tự đọng thực thi lại file khi có thay đổis
+
+// 1:Khai báo biến
+// var const let
+var a="Nguyen Thanh Huy"
+a = 10;
+let b = true
+b = 15;
+// const khong gan lai
+const c = "1000"
+c =1000
+console.log(b);
