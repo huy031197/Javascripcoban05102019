8e6868de48130024894fc7c541489f94f45f44ea
Nguyễn Thanh Huy <nguyenthanhhuy@ThanhHuy.local>
2019-10-06 11:34:18 +0700

    For in, For of

--- buoi2.js
+++ buoi2.js
@@ -119,15 +119,38 @@
  * Viết function trả về tích các số từ start đến end, không tính end 
  * Ví dụ: console.log(calculate(2, 5)) sẽ hiển thị ra màn hình 24 (vì 2 * 3 * 4 = 24)
  */
-function calculate(start, end) {
-    var tich = 1;
-  for (var i = start; i < end ; i++)
-  {
-      tich *= i
-  }
-  return tich
-}
-console.log(calculate(2, 5))
+// function calculate(start, end) {
+//     var tich = 1;
+//   for (var i = start; i < end ; i++)
+//   {
+//       tich *= i
+//   }
+//   return tich
+// }
+// console.log(calculate(2, 5))
+
+// FOR IN , FOR OF
+
+// const huy = {
+//     name : "huy",
+//     age : 25,
+//     location : "Tien Giang"
+// }
+
+// for(var key in huy){
+//     console.log(key)
+// }
+
+// var name = "name"
+// if(name in huy){
+//     console.log("co")
+// }
+
+// const arrayNumbers = [1,2,3,4,5,6]
+// for(var value of arrayNumbers){
+//     console.log(value)
+// }
+
 
 
 
