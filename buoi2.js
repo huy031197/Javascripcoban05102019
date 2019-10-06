8027db507d0e1b525f9d40f8287c0a281bd98441
Nguyễn Thanh Huy <nguyenthanhhuy@ThanhHuy.local>
2019-10-06 09:31:26 +0700

    Arithmetic operator

---
+++ buoi2.js
@@ -0,0 +1,8 @@
+// 3 : Toan tu (Arithmetic operator)
+// ++ -- + - * / %
+// 1 : ++ --
+// 2 : * / %
+// 3 : + -
+var a = 5;
+// a++ : gan truos roi tang gia tri len 1 va nguoc lai
+console.log(++a)
