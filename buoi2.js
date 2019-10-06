6b3c8c59ba75937ff9cc1000c43378047736d0f6
Nguyễn Thanh Huy <nguyenthanhhuy@ThanhHuy.local>
2019-10-06 09:39:35 +0700

    Dien tich tam giac

--- buoi2.js
+++ buoi2.js
@@ -1,8 +1,15 @@
-// 3 : Toan tu (Arithmetic operator)
+// (Arithmetic operator)
+// 3 : Toan
 // ++ -- + - * / %
 // 1 : ++ --
 // 2 : * / %
 // 3 : + -
-var a = 5;
+// var a = 5;
 // a++ : gan truos roi tang gia tri len 1 va nguoc lai
-console.log(++a)
+// console.log(++a)
+// BaiTap1 dien tich tam giac
+// var a = 10;
+// var h = 20;
+// var dt = s;
+// var s = a * h /2;
+// console.log(s)
