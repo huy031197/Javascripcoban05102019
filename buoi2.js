cda062e23e410c43be5194d32cd637dba23a3d68
Nguyễn Thanh Huy <nguyenthanhhuy@ThanhHuy.local>
2019-10-06 09:55:16 +0700

    Phep tinh tang giam

--- buoi2.js
+++ buoi2.js
@@ -1,15 +1,26 @@
-// (Arithmetic operator)
+// (ARITHMETIC OPERATOR)
+
 // 3 : Toan
+
 // ++ -- + - * / %
 // 1 : ++ --
 // 2 : * / %
 // 3 : + -
+
+
 // var a = 5;
 // a++ : gan truos roi tang gia tri len 1 va nguoc lai
 // console.log(++a)
+
 // BaiTap1 dien tich tam giac
 // var a = 10;
 // var h = 20;
 // var dt = s;
 // var s = a * h /2;
 // console.log(s)
+
+// PHEP TINH TANG GIAM
+// var a = 5
+// var b = 6
+// var ketqua = ++a - ++b * --a + --b + b++ * a++ - --a (gan gia tri tung buoc)
+//  console.log(ketqua)
